e07143fbf9de249f6911dd2ee2f52a7d3680c409
Marcis <marcisbergmanis@gmail.com>
2020-06-08 16:39:53 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -25,15 +25,35 @@ function useRoutes(
   return routes[path]({});
 }
 
+function cc(value) {
+  if (Array.isArray(value)) {
+    return value
+      .map(cc)
+      .filter((a) => !!a);
+  }
+
+  if (value && typeof value === 'object') {
+    return Object.entries(value)
+      .map(([key, val]) => val && cc(key))
+      .filter((a) => !!a);
+  }
+
+  return JSON.stringify(value);
+}
+
 export default function Index() {
   let router = useRoutes(routes);
   let [path] = usePath();
+
+  const aboutClass = {
+    active: path === '/about',
+  };
 
   return (
     <div>
       <div>
         <a href="/">Home</a>
-        <a href="/about" class={path === '/about' ? 'active' : ''}>About</a>
+        <a href="/about" class={cc(aboutClass)}>About</a>
         <a href="/users/1">Tom</a>
         <a href="/users/2">Jane</a>
       </div>
