9a8564bd219e1cb9fbed3d8bf1a2911ef3ebfde2
Marcis <marcisbergmanis@gmail.com>
2020-06-08 16:29:53 +0300

    router api

--- app/index.tsx
+++ app/index.tsx
@@ -2,18 +2,38 @@ import SSR from 'ssr-synks';
 
 import routes from './routes';
 
-function useRoutes(routes) {
-  return routes['/']();
+function usePath(): [
+  string,
+  (newPath: string) => void,
+] {
+  const path = '/';
+
+  return [
+    path,
+    (newPath) => { },
+  ];
+}
+
+function useRoutes(
+  routes: { [k: string]: (props) => any },
+  options?: {
+    basePath?: string,
+  },
+) {
+  let [path] = usePath();
+
+  return routes[path]({});
 }
 
 export default function Index() {
   let router = useRoutes(routes);
+  let [path] = usePath();
 
   return (
     <div>
       <div>
         <a href="/">Home</a>
-        <a href="/about">About</a>
+        <a href="/about" class={path === '/about' ? 'active' : ''}>About</a>
         <a href="/users/1">Tom</a>
         <a href="/users/2">Jane</a>
       </div>
