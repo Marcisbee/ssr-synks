3c127ee2baab17e7cba713cacc8278688e41f863
Marcis <marcisbergmanis@gmail.com>
2020-06-08 15:54:03 +0300

    main config

--- src/index.js
+++ src/index.js
@@ -4,23 +4,25 @@ import esbuild from 'esbuild';
 import http from './http';
 import socket from './socket';
 
-const indexPath = resolve('./index.tsx');
+(async () => {
+  const { default: packageJSON } = await import(resolve('./package.json'));
+  const indexPath = resolve(packageJSON.main);
 
-esbuild.build({
-  stdio: 'inherit',
-  entryPoints: [indexPath],
-  outfile: './dist/main.js',
-  minify: false,
-  bundle: true,
-  jsxFactory: 'SSR.h',
-  target: 'esnext',
-  format: 'esm',
-  platform: 'node',
-  external: ['ssr-synks'],
-})
-.then(() => {
-  http();
-  socket();
-})
-.catch(() => process.exit(1));
-
+  esbuild.build({
+    stdio: 'inherit',
+    entryPoints: [indexPath],
+    outfile: './dist/main.js',
+    minify: false,
+    bundle: true,
+    jsxFactory: 'SSR.h',
+    target: 'esnext',
+    format: 'esm',
+    platform: 'node',
+    external: ['ssr-synks'],
+  })
+    .then(() => {
+      http();
+      socket();
+    })
+    .catch(() => process.exit(1));
+})();
