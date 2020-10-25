bec0cf1baa21cb323d3366f99eed018587653326
Marcisbee <marcisbergmanis@gmail.com>
2020-10-25 15:30:51 +0200

    about

---
+++ app/pages/about.tsx
@@ -0,0 +1,11 @@
+import Resync from 'resync';
+
+import { DefaultLayout } from '../layout/default';
+
+export function About() {
+  return (
+    <DefaultLayout>
+      This is about page
+    </DefaultLayout>
+  );
+}
--- app/routes.tsx
+++ app/routes.tsx
@@ -1,11 +1,11 @@
 import Resync from 'resync';
 
 import { Home } from './pages/home';
-import { DefaultLayout } from './layout/default';
+import { About } from './pages/about';
 
 export default {
   '/': () => <Home />,
-  '/about': () => <DefaultLayout>AboutHTML</DefaultLayout>,
+  '/about': () => <About />,
   '/users/:userId': ({ userId }) => `Users ${userId}`,
   '404': () => '404 Error',
 };
